315e77ab1ddfca71001d379702f0a90a610e7a56
oskrgab <ogcg12@gmail.com>
2022-09-18 22:45:37 -0500

    Started with data loading and db creation

--- 1_analysis/load_data.py
+++ 1_analysis/load_data.py
@@ -1,36 +1,15 @@
 import pandas as pd
 import geopandas as gpd
-from utils import preprocess_prod
-#%% unzip data and load csv to dataframe
-drop_cols = [
-    "LEASE",
-    "DOR_CODE",
-    "API_NUMBER",
-    "FIELD",
-    "PRODUCING_ZONE",
-    "OPERATOR",
-    "COUNTY",
-    "TOWNSHIP",
-    "TWN_DIR",
-    "RANGE",
-    "RANGE_DIR",
-    "SECTION",
-    "SPOT",
-    "LATITUDE",
-    "LONGITUDE",
-    "PRODUCT",
-    "URL",
-]
-df = pd.read_csv(
-    "data/oil_lease_production/oil_leases_1980_1989.zip",
-    compression="zip",
-    header=0,
-    parse_dates={"DATE": ["MONTH-YEAR"]},
-    # nrows=1000,
-).drop(drop_cols, axis=1)
-#%% Select dates starting with 0
-df_oil_prod = preprocess_prod(df, "DATE", "PRODUCTION")
+from utils import read_prod_data
+#%% Read oil prod data
+df_oil = read_prod_data("data/oil_lease_production/oil_leases_1980_1989.zip")
+df_gas = read_prod_data("data/gas_lease_production/gas_leases_1980_1989.zip")
 #%% read lease csv
 df_lease = pd.read_csv("data/all_kansas_leases.txt", header=0)
 #%% Read fields shapefile
 fields = gpd.read_file("data/OILGAS_FIELDS_GEO/OILGAS_FIELDS_GEO.shp")
+#%% Read wells csv
+df_wells = pd.read_csv("data/ks_wells.txt", header=0, nrows=1000)
+#%% Read well tops
+df_tops = pd.read_csv("data/ks_tops.txt", header=0, nrows=1000)
+
--- 1_analysis/utils.py
+++ 1_analysis/utils.py
@@ -1,4 +1,26 @@
 import pandas as pd
+from prodphecy import normalize_date_freq
+
+
+DROP_PROD_COLS = [
+    "LEASE",
+    "DOR_CODE",
+    "API_NUMBER",
+    "FIELD",
+    "PRODUCING_ZONE",
+    "OPERATOR",
+    "COUNTY",
+    "TOWNSHIP",
+    "TWN_DIR",
+    "RANGE",
+    "RANGE_DIR",
+    "SECTION",
+    "SPOT",
+    "LATITUDE",
+    "LONGITUDE",
+    "PRODUCT",
+    "URL",
+]
 
 
 def convert_month_year(row, date_col="DATE", prod_col="PRODUCTION"):
@@ -10,21 +32,50 @@ def convert_month_year(row, date_col="DATE", prod_col="PRODUCTION"):
     return x.reset_index().to_numpy()
 
 
-def preprocess_prod(df_prod, date_col, prod_col):
+def preprocess_prod(df_prod, id_col, date_col, wells_col, prod_col):
     # --- Start processing the yearly production data ---
     # Select dates starting with 0
     yearly_dates = df_prod[date_col].str.startswith("0")
     df_year = df_prod[yearly_dates]
-    df_year = df_year.assign(**{date_col: df_year[date_col].str.replace("0", "1", 1)})
-    df_year = (
-        df_year.assign(**{date_col: pd.to_datetime(df_year[date_col])})
-        .apply(convert_month_year, axis=1, args=(date_col, prod_col))
-        .explode()
-    )
-    df_year = pd.DataFrame(df_year.to_list(), columns=df_prod.columns)
+    if len(df_year) > 0:
+        df_year = df_year.assign(**{date_col: df_year[date_col].str.replace("0", "1", 1)})
+        df_year = (
+            df_year.assign(**{date_col: pd.to_datetime(df_year[date_col])})
+            .apply(convert_month_year, axis=1, args=(date_col, prod_col))
+            .explode()
+        )
+        df_year = pd.DataFrame(df_year.to_list(), columns=df_prod.columns)
 
     # --- Process the monthly production data ---
     df = df_prod[~yearly_dates]
     df = df.assign(**{date_col: pd.to_datetime(df[date_col])})
+    df = (
+        df.set_index(date_col)
+        .groupby(id_col)
+        .apply(
+            lambda g: normalize_date_freq(
+                g,
+                "MS",
+                cols_fill_na=[wells_col, prod_col],
+                fill_na=0.0,
+                method_no_cols="ffill",
+            )
+        )
+        .reset_index(0, drop=True)
+        .reset_index()
+    )
     df = pd.concat([df, df_year])
     return df
+
+
+def read_prod_data(data_path):
+    df = (
+        pd.read_csv(
+            data_path,
+            compression="zip",
+            header=0,
+        )
+        .rename({"MONTH-YEAR": "DATE"}, axis=1)
+        .drop(DROP_PROD_COLS, axis=1)
+    )
+    return preprocess_prod(df, "LEASE_KID", "DATE", "WELLS", "PRODUCTION")
