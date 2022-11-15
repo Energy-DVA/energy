b09a4dd2b42ef86b5a22aa0ef1b59df291cf8e2e
oskrgab <ogcg12@gmail.com>
2022-11-14 22:45:41 -0600

    Forecaster finished

---
+++ components/forecaster.py
@@ -0,0 +1,110 @@
+from pmdarima.arima import auto_arima
+import pandas as pd
+from typing import Optional, Union, List
+
+
+class Forecaster:
+    START_P = 0
+    START_Q = 0
+    FIRST_DIFF = 1
+    SEAS_START_P = 0
+    SEAS_START_Q = 0
+    SEAS_FIRST_DIFF = 1
+    PERIODS_SEASON = 12
+    TEST = "adf"
+    SEASONAL = True
+    STEPWISE = True
+    ERROR_ACTION = "ignore"
+    SUPPRESS_WARNINGS = True
+    ALPHA = 0.05
+    # Result columns
+    RES_FORECAST = "forecast"
+    RES_X_VAR = "x_var"
+    RES_LOWER_INTERVAL = "lower_interval"
+    RES_UPPER_INTERVAL = "upper_interval"
+
+    def __init__(
+        self,
+        y: pd.Series,
+        X: Optional[Union[pd.Series, pd.DataFrame, list]] = None,
+    ):
+        """
+        Constructor for Forecaster class.
+
+        Parameters
+        ----------
+        y
+            The time series to forecast. It must be a pandas Series with a
+            DatetimeIndex.
+        X
+            The exogenous variables to use in the forecast. It can be a pandas Series
+            or a pandas DataFrame with a DatetimeIndex.
+        """
+
+        self.y = y
+        self.X = X
+        self.n_periods = None
+        self._model = None
+
+    @property
+    def model(self):
+        if self._model is None:
+            return ValueError("The model has not been fitted yet.")
+
+        return self._model
+
+    @model.setter
+    def model(self, model):
+        self._model = model
+
+    def fit(self):
+        self.model = auto_arima(
+            self.y,
+            X=self.X,
+            start_p=self.START_P,
+            d=self.FIRST_DIFF,
+            start_q=self.START_Q,
+            start_P=self.SEAS_START_P,
+            D=self.SEAS_FIRST_DIFF,
+            start_Q=self.SEAS_START_Q,
+            m=self.PERIODS_SEASON,
+            seasonal=self.SEASONAL,
+            test=self.TEST,
+            stepwise=self.STEPWISE,
+            error_action=self.ERROR_ACTION,
+            suppress_warnings=self.SUPPRESS_WARNINGS,
+        )
+
+    def predict(
+        self, n_periods: int, x_var: Optional[pd.DataFrame] = None
+    ) -> pd.DataFrame:
+        self.n_periods = n_periods
+
+        if self.X is not None and x_var is None:
+            raise ValueError(
+                "The exogenous variables must be provided to make the forecast."
+            )
+
+        forecast, confint = self.model.predict(
+            n_periods=n_periods, X=x_var, return_conf_int=True, alpha=self.ALPHA
+        )
+        # Create a DataFrame with the results
+        df_forecast = pd.DataFrame(
+            {
+                self.RES_FORECAST: forecast.values,
+                self.RES_LOWER_INTERVAL: confint[:, 0],
+                self.RES_UPPER_INTERVAL: confint[:, 1],
+            },
+            index=forecast.index,
+        )
+
+        if x_var is not None:
+            df_forecast[self.RES_X_VAR] = x_var
+
+        return df_forecast
+
+    def fit_predict(
+        self, n_periods: int, x_var: Optional[pd.DataFrame] = None
+    ) -> pd.DataFrame:
+        self.fit()
+        return self.predict(n_periods, x_var)
